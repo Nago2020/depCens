7aa2cebbcc52e9e11171c87374394286bc703cd7
Nago2020 <negaraw@gmail.com>
2024-01-12 21:47:30 +0100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,22 @@
+
+#' @title Follicular Cell Lymphoma
+#'
+#' @description Competing risk data set involving follicular cell lymphoma. The data consist of 541 patients with early disease stage I or II.
+#'
+#'
+#' @name follic
+#' @docType data
+#' @format A data frame with 541 rows and 7 variables:
+#' \itemize{
+#'   \item age:	age
+#'   \item clinstg: clinical stage: 1=stage I, 2=stage II
+#'   \item ch:	chemotherapy
+#'   \item rt:	radiotherapy
+#'   \item hgb: hemoglobin level in g/l
+#'   \item time:	first failure time
+#'   \item status:	censoring status; 0=censored, 1=relapse, 2=death
+#' }
+NULL
+
+
+
